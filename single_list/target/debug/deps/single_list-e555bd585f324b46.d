af335a182ffa9a10c00ea2c317f2e17c30ae885c
VukAmidzic <vuk.amidzic@gmail.com>
2022-10-25 18:01:49 +0200

    new rust package

---
+++ single_list/target/debug/deps/single_list-e555bd585f324b46.d
@@ -0,0 +1,5 @@
+/home/vuk/Desktop/rust_fun/rust_basic_ds/single_list/target/debug/deps/single_list-e555bd585f324b46: src/main.rs
+
+/home/vuk/Desktop/rust_fun/rust_basic_ds/single_list/target/debug/deps/single_list-e555bd585f324b46.d: src/main.rs
+
+src/main.rs:
---
+++ single_list/target/debug/single_list.d
@@ -0,0 +1,1 @@
+/home/vuk/Desktop/rust_fun/rust_basic_ds/single_list/target/debug/single_list: /home/vuk/Desktop/rust_fun/rust_basic_ds/single_list/src/main.rs
