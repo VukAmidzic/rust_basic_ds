6134c3c08825d03cf93bd84424e66c7bb8387396
VukAmidzic <vuk.amidzic@gmail.com>
2022-10-25 19:19:54 +0200

    implementation of single linked list

--- single_list/src/main.rs
+++ single_list/src/main.rs
@@ -1,3 +1,47 @@
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    next: Option<Box<Node>>
+}
+
+impl Node {
+    fn new (value: i32) -> Self {
+        Self {
+            value,
+            next: None
+        }
+    }
+    
+    fn add_to_list(&mut self, val: i32) {
+        match self.next {
+            Some(ref mut x) => {
+                x.add_to_list(val)
+            }
+            None => {
+                self.next = Some(Box::new(Node::new(val)))
+            }
+        }
+    }
+    
+    fn list_output(&mut self) {
+        match self.next {
+            Some(ref mut x) => {
+                print!("{} -> ", self.value);
+                x.list_output()
+            }
+            None => {
+                print!("{}\n", self.value)
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut head: Node = Node::new(1);
+    
+    for i in 2..6 {
+        Node::add_to_list(&mut head, i);
+    }
+    
+    Node::list_output(&mut head);
 }
