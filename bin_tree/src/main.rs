58a9420302c459ed631182215e76b9283cb08d72
VukAmidzic <vuk.amidzic@gmail.com>
2022-10-27 14:29:41 +0200

    added functions for width and rotation

--- bin_tree/src/main.rs
+++ bin_tree/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::cmp::max;
+use std::mem;
 
 #[derive(Debug)]
 struct Node {
@@ -9,6 +10,7 @@ struct Node {
 }
 
 impl Node {
+    //initialization of a node of tree
     fn new(val: i32) -> Self {
         Self {
             value: val,
@@ -17,6 +19,7 @@ impl Node {
         }
     }
     
+    //adding new nodes to a tree
     fn add_to_tree(&mut self, val: i32) {
         if val < self.value {
             match self.left {
@@ -40,6 +43,7 @@ impl Node {
         }
     }
     
+    //making a tree from stdin input
     fn make_list() -> Node {
         let mut numbers = String::new();
 
@@ -62,13 +66,90 @@ impl Node {
         root
     }
     
-    fn get_height(&mut self) {
-        todo!()
+    //function for calculating a height of a tree
+    fn get_height(&mut self) -> u32 {
+        let h_left = match self.left {
+            Some(ref mut l) => {
+                l.get_height()
+            }
+            None => 0
+        };
+        
+        let h_right = match self.right {
+            Some(ref mut r) => {
+                r.get_height()
+            }
+            None => 0
+        };
+        
+        return max(h_left, h_right) + 1;
+    }
+    
+    //helper function for calculating a width
+    //for a certain level of tree
+    fn get_lvl_width(root: &mut Node, lvl: u32) -> u32 {
+        if lvl == 1 {
+            return 1;
+        }
+        else {
+            let w_left = match root.left {
+                Some(ref mut l) => {
+                    Node::get_lvl_width(l, lvl-1)
+                }
+                None => 0
+            };
+            
+            let w_right = match root.right {
+                Some(ref mut r) => {
+                    Node::get_lvl_width(r, lvl-1)
+                }
+                None => 0
+            };
+            
+            return w_right + w_left; 
+        }
+    }
+    
+    //main function for calculating a width of a tree
+    fn get_total_width(root: &mut Node) -> u32 {
+        let h = root.get_height();
+        let mut max_width = 0;
+        let mut width;
+        
+        for i in 1..h+1 {
+            width = Node::get_lvl_width(root, i);
+            if width > max_width {max_width = width};
+        }
+        
+        return max_width;
+    }
+    
+    //mirror-rotating a tree
+    fn rotate_tree(&mut self) {
+        match self.left {
+            Some(ref mut l) => {
+                Node::rotate_tree(l)
+            },
+            None => {}
+        }
+        
+        match self.right {
+            Some(ref mut r) => {
+                Node::rotate_tree(r)
+            },
+            None => {}
+        }
+        
+        mem::swap(&mut self.left, &mut self.right)
     }
 }
 
 fn main() {
-    let mut root = Node::make_list();
+    let mut root: Node = Node::make_list();
     
-    println!("{:?}", root);
+    println!("{:#?}", root);
+    
+    Node::rotate_tree(&mut root);
+    
+    println!("{:#?}", root);
 }
